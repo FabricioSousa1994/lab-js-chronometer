fb0a6a49ae666c10fc299171cff9bd026b4fe613
Fabricio Sousa <fabricio_370@hotmail.com>
2023-03-28 21:47:33 +0100

    solved lab

--- src/chronometer.js
+++ src/chronometer.js
@@ -6,32 +6,32 @@ class Chronometer {
 
   start(printTimeCallback) {
     this.interValId = setInterval(() => {
-    if(printTimeCallback){
-      printTimeCallback()
-    }
-    this.currentTime++
-  }, 1000);  
+      if (printTimeCallback) {
+        printTimeCallback();
+      }
+      this.currentTime++;
+    }, 1000);
   }
 
-  getMinutes(){
+  getMinutes() {
     if (this.currentTime === 0) {
       return 0;
-    }else {
-      return Math.floor(this.currentTime/60);
+    } else {
+      return Math.floor(this.currentTime / 60);
     }
   }
 
   getSeconds() {
-   if(this.currentTime === 0) {
-    return 0;
-   } else {
-    return this.currentTime%60;
-   }
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 60;
+    }
   }
 
   computeTwoDigitNumber(value) {
     if (value.toString().length === 1) {
-      return "0"+value;
+      return "0" + value;
     } else {
       return value.toString();
     }
@@ -46,6 +46,10 @@ class Chronometer {
   }
 
   split() {
-    return this.computeTwoDigitNumber(this.getMinutes()) + ":" + this.computeTwoDigitNumber(this.getSeconds());
+    return (
+      this.computeTwoDigitNumber(this.getMinutes()) +
+      ":" +
+      this.computeTwoDigitNumber(this.getSeconds())
+    );
   }
 }
